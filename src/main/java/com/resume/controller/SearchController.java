b0beeaffa7b825517ade266cae8419f44f93b202
carlosray <carlos.ray@mail.ru>
2020-03-03 10:10:42 +0300

    first commit

---
+++ src/main/java/com/resume/controller/SearchController.java
@@ -0,0 +1,23 @@
+package com.resume.controller;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SearchController extends HttpServlet {
+    private static Logger LOGGER = Logger.getLogger(SearchController.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
